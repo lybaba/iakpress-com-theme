7f362195f70e17935871a8362a6dff6ea4ac26f0
Babaly LY <lybaba@gmail.com>
2026-04-04 00:14:48 +0200

    init npm project

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * IAKPress.com Theme - Functions and definitions
+ *
+ * Note : GeneratePress se charge automatiquement de charger (enqueue) 
+ * le fichier `style.css` de ce thème enfant généré par Tailwind.
+ */
+
+// Ajoutez vos fonctions WordPress personnalisées (shortcodes, filtres, etc.) ci-dessous :
